b2738c4274c685934b4cfe645f4b55602a347e9e
Fang Jian <fangjianmail@126.com>
2013-08-20 21:43:04 +0800

    move download to h2h

--- example/change-html-on-the-fly/html2html.js
+++ example/change-html-on-the-fly/html2html.js
@@ -22,9 +22,42 @@
 	var data2html=function(data,template,adapter){
 		return adapter(data,template);
 	};
+	var wait = function(callbacks, done) {
+	   var counter = callbacks.length;
+	   var next = function() {
+	      if(--counter == 0) {
+	         done();
+	      }
+	   };
+	   for(var i = 0; i < callbacks.length; i++) {
+	      callbacks[i](next);
+	   }
+	};
+	var download=function(link,results){
+		var a = function (next) {
+		   jQuery.get(link, function(res){
+		   		if(res.responseText){
+		   			results[link]=res.responseText;	
+		   		}else{
+		   			results[link]=res;
+		   		}
+	        	next();
+	    	});
+	  	};
+	  	return a;
+  	};
+  	var downloadLinks=function(links,results,done){
+  		var downloaders=[];
+  		for (var i = 0; i < links.length; i++) {
+  			var downloader=download(links[i],results);
+  			downloaders.push(downloader);
+  		};
+  		wait(downloaders,done);
+  	};
 	h2h={
 		html2data:html2data,
-		data2html:data2html
+		data2html:data2html,
+		downloadLinks:downloadLinks
 	};
 	outside.h2h=h2h;
 })(this);
